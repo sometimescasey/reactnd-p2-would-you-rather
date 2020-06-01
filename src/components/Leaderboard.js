99f254690f3d094ee2246d6ee2ea0b8bd61ab618
Casey Li <sometimescasey@gmail.com>
2020-06-01 08:19:54 -0400

    implemented leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import LeaderboardCard from './LeaderboardCard';
 
 class Leaderboard extends Component {
+	sortByTotal = (a,b) => {
+		const { users } = this.props;
+
+		const aTotal = users[a].questions.length + Object.keys(users[a].answers).length;
+
+		const bTotal = users[b].questions.length + Object.keys(users[b].answers).length;
+
+		return bTotal - aTotal;
+	};
+
  	render() {
+ 		const { users } = this.props;
+
  		return (
  			<div className="leaderboard">
  				Leaderboard
- 				{/* TODO: Answered vs Unanswered */}
- 				{/*	Display list of answered vs unanswered qs */}
+ 				<div className="leaderboard-list">
+ 					{Object.keys(users)
+ 						.sort(this.sortByTotal)
+ 						.map((u,i) => (
+ 						<LeaderboardCard
+ 							uid={u}
+ 							key={u}
+ 							rank={i+1}
+ 						/>
+ 						))}
+ 				</div>
  			</div>
  			);
  	}
 }
 
-export default Leaderboard;
-// export default connect()(Leaderboard);
+function mapStateToProps({ users }) {
+	return {
+		users,
+	};
+}
+
+export default connect(mapStateToProps)(Leaderboard);
